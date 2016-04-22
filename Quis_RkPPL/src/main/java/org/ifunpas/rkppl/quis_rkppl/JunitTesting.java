6ed0f22fa2d7565a868b81abaf275dd8d6cf6dd1
khaerul <khaerul.ikhfan@gmail.com>
2016-04-22 17:18:44 +0700

    commit first

---
+++ Quis_RkPPL/src/main/java/org/ifunpas/rkppl/quis_rkppl/JunitTesting.java
@@ -0,0 +1,36 @@
+package org.ifunpas.rkppl.quis_rkppl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JunitTesting {
+	@Before
+		public void test1(){
+			int a, b, c;
+			a=12;
+			b=2;
+			c=a+b;
+		}
+	@Test
+		public void test2(){
+			int a, b, c;
+			a=12;
+			b=2;
+			c=a-b;
+		}
+	@Test
+		public void test3(){
+			int a, b, c;
+			a=12;
+			b=2;
+			c=a*b;
+		}
+	@After
+		public void test4(){
+			int a, b, c;
+			a=12;
+			b=2;
+			c=a/b;
+		}
+}
